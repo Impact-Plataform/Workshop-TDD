4f6e2f652e052ed0dda01362e0260d00233d0555
the-sidh <41924052+the-sidh@users.noreply.github.com>
2021-10-21 21:32:38 -0300

    Update Baskara.kt

--- src/main/kotlin/tdd/quadratic/baskara/Baskara.kt
+++ src/main/kotlin/tdd/quadratic/baskara/Baskara.kt
@@ -3,5 +3,5 @@ package tdd.quadratic.baskara
 import java.math.BigDecimal
 
 class Baskara {
-    fun calculate(a: BigDecimal, b: BigDecimal, c: BigDecimal): List<BigDecimal> = listOf(BigDecimal.ONE)
+    fun calculate(a: BigDecimal, b: BigDecimal, c: BigDecimal): Set<BigDecimal> = setOf(BigDecimal.ONE)
 }
