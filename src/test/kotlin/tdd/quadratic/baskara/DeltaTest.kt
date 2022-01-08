131643f0c1c6ce0707c58bf03d2384822d9f849e
Alentejo <arthur.alentejo228@gmail.com>
2022-01-08 10:54:30 -0300

    Criando o teste de Delta

--- src/test/kotlin/tdd/quadratic/baskara/DeltaTest.kt
+++ src/test/kotlin/tdd/quadratic/baskara/DeltaTest.kt
@@ -1,4 +1,19 @@
 package tdd.quadratic.baskara
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
 class DeltaTest {
+
+    @Test
+    fun `deve retornar 49`() {
+        val a = BigDecimal(1)
+        val b = BigDecimal(-1)
+        val c = BigDecimal(-12)
+        val expected = 49
+
+        val result = Delta.calculate(a, b, c)
+        Assertions.assertEquals(expected,result)
+    }
 }
