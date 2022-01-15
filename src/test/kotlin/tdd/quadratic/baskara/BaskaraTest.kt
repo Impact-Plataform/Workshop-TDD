77052d870fb7bb90dd67cebe617209f0de5e4e2b
Plataforma Impact <87543326+Impact-Plataform@users.noreply.github.com>
2022-01-15 08:20:40 -0300

    Update BaskaraTest.kt

--- src/test/kotlin/tdd/quadratic/baskara/BaskaraTest.kt
+++ src/test/kotlin/tdd/quadratic/baskara/BaskaraTest.kt
@@ -1,4 +1,20 @@
 package tdd.quadratic.baskara
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
 class BaskaraTest {
+
+  @Test
+  fun `deve retornar 4 e -3`(){
+    val a = BigDecimal(1)
+    val b = BigDecimal(-1)
+    val c = BigDecimal(-12)
+    val expected = setOf<BigDecimal>(BigDecimal(4), BigDecimal(-3))
+
+    val result = Baskara().calculate(a, b, c)
+    Assertions.assertEquals(expected, result)
+  }
+
 }
