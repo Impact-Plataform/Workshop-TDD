3a547e9b2f19c26b51c9dd5fce2e549715454ad8
Plataforma Impact <87543326+Impact-Plataform@users.noreply.github.com>
2022-01-15 08:20:55 -0300

    Update Baskara.kt

--- src/main/kotlin/tdd/quadratic/baskara/Baskara.kt
+++ src/main/kotlin/tdd/quadratic/baskara/Baskara.kt
@@ -1,7 +1,18 @@
 package tdd.quadratic.baskara
 
 import java.math.BigDecimal
+import java.math.MathContext
 
 class Baskara {
-    fun calculate(a: BigDecimal, b: BigDecimal, c: BigDecimal): Set<BigDecimal> = setOf(BigDecimal.ONE)
+    fun calculate(a: BigDecimal, b: BigDecimal, c: BigDecimal): Set<BigDecimal> {
+        val twoTimesA = a.multiply(BigDecimal(2))
+        val squareRootDelta = Delta.calculate(a,b,c).sqrt(MathContext(10))
+        val minusB = b.negate()
+
+        return setOf(
+            minusB.plus(squareRootDelta).divide(twoTimesA),
+            minusB.minus(squareRootDelta).divide(twoTimesA)
+        )
+
+    }
 }
