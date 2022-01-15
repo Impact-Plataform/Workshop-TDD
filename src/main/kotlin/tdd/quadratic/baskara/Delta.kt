ddf7c286c1a7862175fbf4d395f9dc436e8cb71f
Plataforma Impact <87543326+Impact-Plataform@users.noreply.github.com>
2022-01-15 08:33:42 -0300

    Update Delta.kt

--- src/main/kotlin/tdd/quadratic/baskara/Delta.kt
+++ src/main/kotlin/tdd/quadratic/baskara/Delta.kt
@@ -4,6 +4,10 @@ import java.math.BigDecimal
 
 class Delta {
     companion object{
-        fun calculate(a :BigDecimal , b: BigDecimal, c: BigDecimal): BigDecimal = BigDecimal.ONE
+        fun calculate(a :BigDecimal , b: BigDecimal, c: BigDecimal): BigDecimal {
+            val powB = b.pow(2)
+            val fourTimesAC = BigDecimal(4).multiply(a).multiply(c)
+            return powB.minus(fourTimesAC)
+        }
     }
 }
